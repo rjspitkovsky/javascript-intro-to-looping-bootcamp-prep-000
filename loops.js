3b9efa73b6ed354616662b303f879e2b35220d1e
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-11-14 19:21:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,6 @@
-for (var i =1; i < 100: i++) {
-  console.log("Hello World the" + i + "time")
+function forLoop(array) {
+  for (var x = 0, x < 25, x++) {
+    array.push(`I am ${x} strange loops`)
+  }
+  return array 
 }
