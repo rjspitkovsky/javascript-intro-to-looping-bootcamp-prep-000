3b261ce03231dfcd2f9680f4f15524f25337e91b
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-11-14 19:32:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,4 @@
 array = []
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.unshift("I am 1 strange loop.")
-    }; else {
-      array.unshift(`I am ${i} strange loops.`)
-    }
-  }
-  return array
+  
 }
