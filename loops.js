c036728f146cff6f17ae764d18283dbca08a4a32
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-11-14 19:56:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,6 +18,12 @@ function whileLoop(number) {
   }
 }
 
-function doWhileLoop(array) {
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
 
+function doWhileLoop(array) {
+do {
+  array.shift()
+} while array.length > 0 && maybeTrue()
 }
